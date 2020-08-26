13a3a1448f51db3de7c3ce85237efef1cfc36084
ksenkso <yazunya@gmail.com>
2020-08-26 18:03:26 +0300

    fix: use CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(3000);
 }
 bootstrap();
