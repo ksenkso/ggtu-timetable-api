e6df0cfd3af24f48508077f37282bb3017d7b6af
ksenkso <yazunya@gmail.com>
2020-10-03 19:10:35 +0300

    feature: moved hash function to the class definition so it can be imported

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,46 +1,57 @@
-import { BeforeSave, BelongsToMany, Column, Model, Table } from 'sequelize-typescript';
+import { BeforeSave, BelongsTo, BelongsToMany, Column, ForeignKey, Model, Table } from 'sequelize-typescript';
 import * as bcrypt from 'bcrypt';
 import { Role } from './role.model';
 import { UserRole } from './user-role.model';
+import { Faculty } from './faculty.model';
 
-const hashPassword = async (password) => {
-    if (!password) {
-        throw new Error('Password shouldn\'t be an empty string.');
-    } else {
-        const salt = await bcrypt.genSalt(10);
-        return await bcrypt.hash(password, salt);
-    }
-};
-@Table
+@Table({
+  timestamps: false
+})
 export class User extends Model<User> {
-    @Column
-    username: string
-
-    @Column
-    password: string
-
-    @BelongsToMany(() => Role, () => UserRole)
-    roles: Role[]
-
-    @BeforeSave
-    static async updatePassword(model: User) {
-        if (model.changed('password')) {
-            model.password = await hashPassword(model.password);
-        }
+  static async hashPassword(password: string) {
+    if (!password) {
+      throw new Error('Password shouldn\'t be an empty string.');
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      return await bcrypt.hash(password, salt);
     }
+  }
 
-    async comparePassword(pw) {
-        try {
-            return await bcrypt.compare(pw, this.password);
-        } catch (e) {
-            console.error(JSON.stringify(e));
-            return false;
-        }
-    }
+  @Column
+  username: string;
 
-    withoutPassword() {
-        const json = this.toJSON() as any;
-        delete json.password;
-        return json;
+  @Column
+  password: string;
+
+  @ForeignKey(() => Faculty)
+  @Column
+  facultyId: number;
+
+  @BelongsTo(() => Faculty)
+  faculty: Faculty;
+
+  @BelongsToMany(() => Role, () => UserRole)
+  roles: Role[];
+
+  @BeforeSave
+  static async updatePassword(model: User) {
+    if (model.changed('password')) {
+      model.password = await User.hashPassword(model.password);
     }
+  }
+
+  async comparePassword(pw) {
+    try {
+      return await bcrypt.compare(pw, this.password);
+    } catch (e) {
+      console.error(JSON.stringify(e));
+      return false;
+    }
+  }
+
+  withoutPassword() {
+    const json = this.toJSON() as any;
+    delete json.password;
+    return json;
+  }
 }
