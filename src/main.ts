fa2438149c8bbb36f78764abb1994eced0c3c02f
ksenkso <yazunya@gmail.com>
2020-10-03 19:11:07 +0300

    feature: use ValidationPipe globally to transform all incoming messages

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
 dotenv.config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   await app.listen(3000);
 }
+
 bootstrap();
